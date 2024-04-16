e3b4b93b5b3f5376398b2f51cd0f175eef90a0fa
jagudeloq <jagudeloq@COR-DLLOSOFT34>
2024-04-16 10:22:26 -0500

    Variable de entorno

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 import { config } from "dotenv";
 config();
 
-export const MERCADOPAGO_API_KEY = "TEST-5983342333018105-041512-0df6afbc6d701f8300d84bd376012df2-1772584324"
-//process.env.MERCADOPAGO_API_KEY;
+export const MERCADOPAGO_API_KEY = process.env.MERCADOPAGO_API_KEY;
